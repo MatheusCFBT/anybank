273473ef63a627df27843f9bd9428a95de55a614
MatheusCFBT <matheusterzini@gmail.com>
2025-11-23 20:58:32 -0300

    first exercize

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,6 +1,13 @@
 void main() {
   var contaMatheus = Conta("Matheus", 1000);
-  var contaRoberta = Conta("Roberta", 5000);
+  var contaRoberta = Conta("Roberta", 2000);
+  var contas = <Conta>[contaMatheus, contaRoberta];
+  for (var conta in contas) {
+    print(conta.owner);
+    print(conta.balance);
+  }
+  contaRoberta.balance = 5000;
+  print(contaRoberta.balance);
 }
 
 class Conta {
